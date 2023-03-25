5acd9de7307a8cd94655e0bb1f84416043cdcf77
Ahmed Khalil <ahmad.khalil@numumail.com>
2023-03-25 14:33:32 +0500

    added mongo url for .env and fileupload fix

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,67 +1,62 @@
-const express = require("express");
-const multer = require("multer");
-const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
-const { promisify } = require("util");
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const { promisify } = require('util');
 
-const pipeline = promisify(require("stream").pipeline);
+const pipeline = promisify(require('stream').pipeline);
 
 const router = express.Router();
 
 const upload = multer();
 
-router.post("/resume", upload.single("file"), (req, res) => {
+router.post('/resume', upload.single('file'), (req, res) => {
   const { file } = req;
-  if (file.detectedFileExtension != ".pdf") {
+  if (!['application/pdf'].includes(file.mimetype)) {
     res.status(400).json({
-      message: "Invalid format",
+      message: 'Invalid format'
     });
   } else {
-    const filename = `${uuidv4()}${file.detectedFileExtension}`;
-
+    const filename = `${uuidv4()}${file.mimetype.replace('application/', '.')}`;
     pipeline(
-      file.stream,
+      `${file.buffer}`,
       fs.createWriteStream(`${__dirname}/../public/resume/${filename}`)
     )
       .then(() => {
         res.send({
-          message: "File uploaded successfully",
-          url: `/host/resume/${filename}`,
+          message: 'File uploaded successfully',
+          url: `/host/resume/${filename}`
         });
       })
       .catch((err) => {
         res.status(400).json({
-          message: "Error while uploading",
+          message: 'Error while uploading'
         });
       });
   }
 });
 
-router.post("/profile", upload.single("file"), (req, res) => {
+router.post('/profile', upload.single('file'), (req, res) => {
   const { file } = req;
-  if (
-    file.detectedFileExtension != ".jpg" &&
-    file.detectedFileExtension != ".png"
-  ) {
+  if (!['image/jpg', 'image/png', 'image/jpeg'].includes(file.mimetype)) {
     res.status(400).json({
-      message: "Invalid format",
+      message: 'Invalid format'
     });
   } else {
-    const filename = `${uuidv4()}${file.detectedFileExtension}`;
-
+    const filename = `${uuidv4()}${file.mimetype.replace('image/', '.')}`;
     pipeline(
-      file.stream,
+      `${file.buffer}`,
       fs.createWriteStream(`${__dirname}/../public/profile/${filename}`)
     )
       .then(() => {
         res.send({
-          message: "Profile image uploaded successfully",
-          url: `/host/profile/${filename}`,
+          message: 'Profile image uploaded successfully',
+          url: `/host/profile/${filename}`
         });
       })
       .catch((err) => {
         res.status(400).json({
-          message: "Error while uploading",
+          message: 'Error while uploading'
         });
       });
   }
